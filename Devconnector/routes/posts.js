ba3e728675c12be3c813c81f459e0ac43600dbe6
mohit chopra <mohitchopra5743@gmail.com>
2023-05-15 14:47:54 +0530

    init

--- Devconnector/routes/posts.js
+++ Devconnector/routes/posts.js
@@ -35,4 +35,61 @@ const Post= require('../models/Posts')
      }
    }
  );
+
+ router.get('/', auth, async (req, res) => {
+   try {
+     const posts = await Post.find().sort({ date: -1 });
+     return res.json(posts);
+   } catch (err) {
+     console.error(err.message);
+     res.status(500).send('Server Error');
+   }
+ });
+
+ router.get('/:_id', auth, async (req, res) => {
+   try {
+     const post = await Post.findById(req.params._id);
+ 
+     if (!post) {
+       return res.status(404).json({ msg: 'Post not found' });
+     }
+ 
+     res.json(post);
+   } catch (err) {
+     console.error(err.message);
+
+     if (err.kind=== 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+ 
+     res.status(500).send('Server Error');
+   }
+ });
+
+ router.delete('/:_id', auth, async (req, res) => {
+   try {
+     const post = await Post.findById(req.params._id);
+ 
+     if (!post) {
+       return res.status(404).json({ msg: 'Post not found' });
+     }
+ 
+     // Check user
+     if (post.user.toString() !== req.user.id) {
+       return res.status(401).json({ msg: 'User not authorized' });
+     }
+     await post.deleteOne();
+ 
+     res.json({ msg: 'Post removed' });
+   } catch (err) {
+     console.error(err.message);
+
+    // if (err.kind=== 'ObjectId') {
+      //return res.status(404).json({ msg: 'Post not found' });
+    //}
+ 
+     res.status(500).send('Server Error');
+   }
+ });
  module.exports=router;
