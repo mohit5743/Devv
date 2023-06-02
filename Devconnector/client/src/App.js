71750850ef1f41479d67acebb4e257c8e10d0e44
mohit chopra <mohitchopra5743@gmail.com>
2023-06-02 19:39:13 +0530

    init

--- Devconnector/client/src/App.js
+++ Devconnector/client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Fragment} from 'react';
-import {BrowserRouter as Router,Routes, Route,Link} from 'react-router-dom';
+import {BrowserRouter as Router,Routes, Route} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import './App.css';
 import Landing from './components/layout/Landing';
