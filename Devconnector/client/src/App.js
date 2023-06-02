a9665f6cc35ccb6bb167faa2dd051149c63dcc17
mohit chopra <mohitchopra5743@gmail.com>
2023-06-02 18:29:24 +0530

    init

--- Devconnector/client/src/App.js
+++ Devconnector/client/src/App.js
@@ -1,16 +1,22 @@
 import React, { Fragment} from 'react';
-import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {BrowserRouter as Router,Routes, Route,Link} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import './App.css';
 import Landing from './components/layout/Landing';
-
-const App =()  => (
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+const App = () => (
   <Router>
-    <Fragment>   
-    <Navbar />
-    <Route exact path="/" component={ Landing } />
+    <Fragment> 
+      <Navbar />
+      <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      </Routes>
     </Fragment>
   </Router>
 );
+
 
 export default App;
